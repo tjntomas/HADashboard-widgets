a97a1c9093e00bb046241dcd50f0a8e7f4dd49c8
Tomas Jansson <31620886+tjntomas@users.noreply.github.com>
2019-03-20 21:24:08 +0100

    Update basehagraph.js

--- custom_widgets/basehagraph/basehagraph.js
+++ custom_widgets/basehagraph/basehagraph.js
@@ -427,6 +427,6 @@ function basehagraph(widget_id, url, skin, parameters)
 			time = m[1]
 			today.setTime(today.getTime() - sec * m[0])
 		}
-		return today.toLocaleDateString() + "T" + today.toLocaleTimeString()
+		return today.toLocaleDateString("sv-SE") + "T" + today.toLocaleTimeString()
 	}
 }
