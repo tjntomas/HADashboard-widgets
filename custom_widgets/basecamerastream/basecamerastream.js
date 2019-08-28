a94ab19f31c58049dcc0ed31e4471d938e30e03a
Tomas Jansson <31620886+tjntomas@users.noreply.github.com>
2019-08-28 02:26:26 +0200

    Add files via upload

---
+++ custom_widgets/basecamerastream/basecamerastream.js
@@ -0,0 +1,49 @@
+function basecamerastream(widget_id, url, skin, parameters)
+{
+    self = this
+    
+    self.parameters = parameters;
+    
+    var callbacks = []
+
+    self.OnStateAvailable = OnStateAvailable;
+    self.OnStateUpdate = OnStateUpdate;
+    
+    var monitored_entities = 
+        [
+            {"entity": parameters.entity, "initial": self.OnStateAvailable, "update": self.OnStateUpdate}
+        ]; 
+
+    self.log = log
+
+    WidgetBase.call(self, widget_id, url, skin, parameters, monitored_entities, callbacks);
+
+    function OnStateAvailable(self, state)
+    {        
+        set_view(self, state)
+    }
+    
+    function OnStateUpdate(self, state)
+    {
+        console.log("New access token: ", state.attributes['access_token'])
+        self.set_view(self, state)
+    }
+ 
+    function set_view(self, state)
+    {
+        var stream_url = self.parameters.base_url + "/api/camera_proxy_stream/" + state.entity_id  + "?token=" + state.attributes["access_token"]
+        self.log("Stream url:", stream_url)
+        self.set_field(self, "img_internal_src", ""); // Needed to prevent a 504 server error.
+        self.set_field(self, "img_internal_src", stream_url);
+    }
+
+    function log(){
+        if ("log" in this.parameters){
+            var msg = ""
+            for (s of arguments){
+                msg = msg + " " + s
+            }
+            console.info(msg)
+        }
+    }
+}
