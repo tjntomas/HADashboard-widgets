82c7f43d74cf8c58019b50c552c6e07d1a87a2d0
Tomas Jansson <31620886+tjntomas@users.noreply.github.com>
2020-02-03 18:24:02 +0100

    Add files via upload

--- custom_widgets/basedocker/basedocker.js
+++ custom_widgets/basedocker/basedocker.js
@@ -12,38 +12,65 @@ function basedocker(widget_id, url, skin, parameters)
         ]
 
     callbacks = []
-    
-    self.stop_icon      = elem(self, "mdi mdi-stop-circle-outline")
-    self.play_icon      = elem(self, "mdi mdi-play-circle-outline")
-    self.restart_icon   = elem(self, "mdi mdi-restart")
-    self.uptime         = elem(self, "uptime")
-    self.state          = elem(self, "state")
-    self.host           = elem(self, "host")
+    EL_STOP         = "mdi mdi-stop-circle-outline"
+    EL_PLAY         = "mdi mdi-play-circle-outline"
+    EL_RESTART      = "mdi mdi-restart"
+    EL_UPTIME       = "uptime"
+    EL_STATE        = "state"
+    EL_HOST         ="host" 
+    EL_CONTAINER    = "container"
+    EV_CLICK        = "click"
+    EV_MOUSEOVER    = "mouseover"
+    EV_MOUSEOUT     = "mouseout"
 
-    self.stop_icon.addEventListener("click", btn_stop.bind(self), true)
-    self.play_icon.addEventListener("click", btn_play.bind(self), true)
-    self.restart_icon.addEventListener("click", btn_restart.bind(self), true)
+    self.stop_icon      = elem(self, EL_STOP)
+    self.play_icon      = elem(self, EL_PLAY)
+    self.restart_icon   = elem(self, EL_RESTART)
+    self.uptime         = elem(self, EL_UPTIME)
+    self.state          = elem(self, EL_STATE)
+    self.host           = elem(self, EL_HOST)
+    self.container      = elem(self, EL_CONTAINER)
+
+    self.stop_icon.addEventListener(EV_CLICK, btn_stop.bind(self), true)
+    self.play_icon.addEventListener(EV_CLICK, btn_play.bind(self), true)
+    self.restart_icon.addEventListener(EV_CLICK, btn_restart.bind(self), true)
+    self.container.addEventListener(EV_MOUSEOVER, mouseover.bind(self), true)
+    self.container.addEventListener(EV_MOUSEOUT, mouseout.bind(self), true)
 
     WidgetBase.call(self, widget_id, url, skin, parameters, monitored_entities, callbacks)
+
+    self.icon_colors = {}
     
+    async function mouseover(e){
+        this.icon_colors[e.target] = e.target.style.color 
+        e.target.style.color = "rgba(60,60,255)"
+    }
+
+    async function mouseout(e){
+        e.target.style.color = this.icon_colors[e.target]
+    }
+
     async function btn_stop(){
         this.call_service(this, this.parameters.post_service_stop)
         this.set_field(this, "stop_style", "color: orange")
-        this.state.innerHTML = "Stopping"
+        this.icon_colors[this.stop_icon] = "orange"
+        this.state.innerHTML = "Stopping..."
         await blink(this.state, this.stop_icon,  this.state.innerHTML)
     }
 
     async function btn_play(){
         this.call_service(this, this.parameters.post_service_start)
         this.set_field(this, "play_style", "color: orange")
-        this.state.innerHTML = "Starting"
+        this.icon_colors[this.play_icon] = "orange"
+        this.state.innerHTML = "Starting..."
         await blink(this.state, this.play_icon,  this.state.innerHTML)
     }
 
     async function btn_restart(){
         this.call_service(this, this.parameters.post_service_restart)
         this.set_field(this, "restart_style", "color: orange")
-        this.state.innerHTML = "Restarting"
+        this.icon_colors[this.restart_icon] = "orange"
+        this.state.innerHTML = "Restarting..."
         await blink(this.state, this.restart_icon,  this.state.innerHTML)
     }
 
@@ -58,8 +85,7 @@ function basedocker(widget_id, url, skin, parameters)
 
     async function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms))
-      }
-
+    }
 
     function OnStateAvailable(self, state){        
         set_view(self,state)    
