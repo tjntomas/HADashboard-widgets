bba63f09fcd6123695bc256ea3c6753b5276ce5e
Tomas Jansson <31620886+tjntomas@users.noreply.github.com>
2021-01-13 19:44:29 +0100

    Async fetch
    Change to async fetch of container data.

--- apps/dock.py
+++ apps/dock.py
@@ -3,6 +3,8 @@ from requests import get
 from requests import post
 import json
 import datetime
+import asyncio
+import aiohttp
 
 """ App to manage docker containers. 
     The app extracts data from the docker API and creates sensors
@@ -19,9 +21,9 @@ class Docker(hass.Hass):
     EVNT_DOCKER_STOP        = "docker_stop"
     EVNT_DOCKER_RESTART     = "docker_restart"
     DOCKER_SIGNAL_SENSOR    = "sensor.docker_signal"
-    SERVICE_MAP             = {EVNT_DOCKER_START, "start", EVNT_DOCKER_STOP, "stop", EVNT_DOCKER_RESTART, "restart"}
+    SERVICE_MAP             = {EVNT_DOCKER_START: "start", EVNT_DOCKER_STOP: "stop", EVNT_DOCKER_RESTART: "restart"}
 
-    def initialize(self):  
+    async def initialize(self):  
         self.register_service(self.SVC_DOCKER_START, self.docker_manage)
         self.register_service(self.SVC_DOCKER_STOP, self.docker_manage)
         self.register_service(self.SVC_DOCKER_RESTART, self.docker_manage)
@@ -31,46 +33,40 @@ class Docker(hass.Hass):
         self.set_state(self.DOCKER_SIGNAL_SENSOR, state="none")
         self.listen_state(self.docker_signal, self.DOCKER_SIGNAL_SENSOR)
         runtime = datetime.time(0, 0, 0)
+        await self.get_containers({})
         self.run_minutely(self.get_containers, runtime)
 
     async def docker_signal(self, entity, attribute, old, new, kwargs):
         entity_id = await self.get_state(self.DOCKER_SIGNAL_SENSOR, attribute="entity_id")
         service = await self.get_state(self.DOCKER_SIGNAL_SENSOR, attribute="service")
-        self.log("Logging")
         self.log(entity_id) 
         self.log(service)
         if service != "":
             await self.docker_events("docker_" + service,{"entity_id": entity_id}, "")
 
     async def docker_events(self, event_name, data, kwargs):
-        self.log(data)
         if 'entity_id' in data:
             c_id = await self.get_state(data['entity_id'], attribute='id')
-            host = await self.get_state(data['entity_id'], attribute='host')
+            host = await self.get_state(data['entity_id'], attribute='ip')
             url = self.URL_TEMPLATE.format(host, c_id, self.SERVICE_MAP[event_name])           
             post(url=url)
             await self.sleep(1)
-            self.get_containers("")
+            await self.get_containers("")
 
     async def docker_manage(self, plugin, domain, service, data):
-        self.log(domain)
-        self.log(service)
-
         container   = await self.get_state(data['entity_id'], attribute='container')
-        host        = await self.get_state(data['entity_id'], attribute='host')
+        host        = await self.get_state(data['entity_id'], attribute='ip')
         c_id        = await self.get_state(data['entity_id'], attribute='id')
 
         self.log(service + ": " + container)
-
         self.URL_TEMPLATE.format(host, c_id, service)
-        self.log(url)
         p = post(url=url)
-        self.log("Action done")
         await self.sleep(1)
         self.get_containers("") 
 
-    def get_containers(self, kwargs):
+    async def get_containers(self, kwargs):
         QUERY           = 'v1.24/containers/json?all=1'
+        CONTAINER_STATS = 'v1.24/containers/{}/stats?stream=false'
         STATE           = 'State'
         NAMES           = 'Names'
         RUNNING         = 'running'
@@ -84,6 +80,7 @@ class Docker(hass.Hass):
         ATTR_STATE      = 'state'
         ATTR_CONTAINER  = 'container'
         ATTR_TIMESTAMP  = 'last_updated'
+        ATTR_IP         = 'ip'
         ATTR_ICON       = 'icon'
         ATTR_ID         = 'id'
         DOCKER_DOMAIN   = 'docker.'
@@ -94,36 +91,42 @@ class Docker(hass.Hass):
 
         names = []
         for item  in self.args[HOSTS]:
-            url = URL_TEMPLATE.format(item, self.args[HOSTS][item], QUERY)
+            host = self.args[HOSTS][item]['host']
+            port = self.args[HOSTS][item]['port']
+            host_name = self.args[HOSTS][item]['name']
+            url = URL_TEMPLATE.format(host, port, QUERY)
+            self.log("Fetching container data from " +url)
+
             try:
-                response = get(url)
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url) as response:
+                        r=await response.text()
             except:
                 self.log(ERR_RESPONSE)
             else:
                 try:
-                    js = json.loads(response.text)
+                    js = json.loads(r)
                 except:
                     self.log(ERR_JSON)
                 else:
                     for entry in js:
                         args = {}
                         name = str(entry[NAMES]).replace("/", "").replace("'", "").replace("[", "").replace("]","").replace("-", "_").replace(" ", "").replace(",","").lower()
-                        self.log(str(name))
                         org_name =  name
                         cnt = 1
                         n = name
                         while n in names:
-                            n = name + "_" + str(cnt)
+                            n = name + "_" + item 
                             cnt = cnt + 1
                         names.append(n)
-                        name = n
+                        name = n.replace(".","_")
                         state = entry[STATE]
                         if state == RUNNING:
                             state = STATE_ON
                         else:
-                            state = STATE_OFF
+                            state = STATE_OFF 
                         image = entry[IMAGE]
-                        status = str(entry['Status']).replace("Up ", "").replace("Exited (137)", "Stopped ")
+                        status = str(entry['Status']).replace("Up ", "").replace("Exited (137)", "Stopped ").replace("(healthy)","")
                         args[ATTR_UPTIME] = status
                         args[ATTR_IMAGE] = image
                         args[ATTR_HOST] = item
@@ -131,6 +134,6 @@ class Docker(hass.Hass):
                         args[ATTR_STATE] = str(entry[STATE]).replace("exited", "Stopped").replace("running","Running")
                         args[ATTR_CONTAINER] = org_name
                         args[ATTR_TIMESTAMP] = str(datetime.datetime.now())
+                        args[ATTR_IP] = host
                         args[ATTR_ICON] = DOCKER_ICON
                         self.set_state(DOCKER_DOMAIN + name, state=state, attributes=args)
-       
